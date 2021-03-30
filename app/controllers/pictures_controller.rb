c21c067d1b4d4b0c66cd7eedda0c7bfa65edafdf
yuki ohno <jeanadler33@gmail.com>
2021-03-30 07:02:04 +0000

    pictureのCRUD機能の微修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,16 +1,13 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: %i[ show edit update destroy ]
 
-  # GET /pictures or /pictures.json
   def index
     @pictures = Picture.all
   end
 
-  # GET /pictures/1 or /pictures/1.json
   def show
   end
 
-  # GET /pictures/new
   def new
     if params[:back]
       @picture = Picture.new(picture_params)
@@ -24,11 +21,9 @@ class PicturesController < ApplicationController
     render :new if @picture.invalid?
   end
 
-  # GET /pictures/1/edit
   def edit
   end
 
-  # POST /pictures or /pictures.json
   def create
     @picture = current_user.pictures.build(picture_params)
 
@@ -43,7 +38,6 @@ class PicturesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pictures/1 or /pictures/1.json
   def update
     respond_to do |format|
       if @picture.update(picture_params)
@@ -56,7 +50,6 @@ class PicturesController < ApplicationController
     end
   end
 
-  # DELETE /pictures/1 or /pictures/1.json
   def destroy
     @picture.destroy
     respond_to do |format|
@@ -66,12 +59,10 @@ class PicturesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_picture
       @picture = Picture.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def picture_params
       params.require(:picture).permit(:title, :content, :image, :image_cache, :user_id)
     end
