1cad03103f6d0fada36eaedd826cced7ac9725e3
yuki ohno <jeanadler33@gmail.com>
2021-03-31 04:38:54 +0000

    検証のためルートに設定していたpicturesコントローラのindexアクションを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  root 'pictures#index'
   resources :pictures do
     collection do
       post :confirm
